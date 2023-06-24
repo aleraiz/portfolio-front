3cae62acef92c5813140e7bb797a8199ff08336a
aleraiz <alejandroraiz7@gmail.com>
2023-06-24 18:29:43 -0300

    static json and form animation

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,10 +1,23 @@
-import Image from "next/image";
-import Link from "next/link";
-import pplWorking from "../public/drawingof2softwar.jpg";
+import { useEffect, useRef } from "react";
+import { motion, useInView, useAnimation } from "framer-motion";
+import statics from "../static/statics.json";
+console.log(statics.styles.formInputs);
+
 export default function Contact() {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
+  const formControl = useAnimation();
+  useEffect(() => {
+    console.log(isInView);
+    if (isInView) {
+      // Fire the animation
+      formControl.start("visible");
+    }
+  }, [isInView]);
+
   return (
     <div
-      // className="w-full lg:min-h-screen px-32 border border-light py-8 lg:py-20 section"
       className="w-full min-h-screen border border-light px-8 md:px-20 pt-32 md:pt-20 py-8 lg:py-20 "
       id="contact"
     >
@@ -13,18 +26,30 @@ export default function Contact() {
       </div>
       <div className="flex flex-col md:flex-row items-center justify-center min-h-[60vh] ">
         <div className="flex flex-col items-center justify-center h-full my-auto">
-          <form className="flex w-full  space-x-3">
+          <form className="flex w-full space-x-3">
             <div className="w-full max-w-6xl px-5 py-2 md:py-10 m-auto  bg-dark rounded-lg shadow dark:bg-gray-800">
-              <div className="mb-6  font-light text-center text-light dark:text-white">
+              <div
+                ref={ref}
+                className="mb-6  font-light text-center text-light dark:text-white"
+              >
                 Want to work together or have any questions?
               </div>
-              <div className="grid max-w-2xl grid-cols-2 gap-4 m-auto mt-10">
+              <motion.div
+                className="grid max-w-2xl grid-cols-2 gap-4 m-auto mt-10"
+                variants={{
+                  hidden: { opacity: 0, y: 200 },
+                  visible: { opacity: 1, y: 0 },
+                }}
+                initial="hidden"
+                animate={formControl}
+                transition={{ duration: 1, delay: 0.25 }}
+              >
                 <div className="col-span-2 lg:col-span-1">
                   <div className=" relative ">
                     <input
                       type="text"
                       id="contact-form-name"
-                      className=" border-transparent flex-1 appearance-none  w-full py-2 px-4 bg-slate-800 border-gray-700 text-gray-400 placeholder-gray-500 focus:ring-2 focus:outline-none focus:ring-gray-700 shadow-slate-600 text-base  focus:outline-double"
+                      className={statics.styles.formInputs}
                       placeholder="Name"
                     />
                   </div>
@@ -34,7 +59,7 @@ export default function Contact() {
                     <input
                       type="text"
                       id="contact-form-email"
-                      className=" border-transparent flex-1 appearance-none  w-full py-2 px-4 bg-slate-800 border-gray-700 text-gray-400 placeholder-gray-500 focus:ring-2 focus:outline-none focus:ring-gray-700 shadow-slate-600 text-base  focus:outline-double"
+                      className={statics.styles.formInputs}
                       placeholder="email"
                     />
                   </div>
@@ -42,7 +67,7 @@ export default function Contact() {
                 <div className="col-span-2">
                   <label className="text-gray-700" htmlFor="name">
                     <textarea
-                      className="rounded-sm border-transparent flex-1 appearance-none border w-full py-2 px-4 bg-slate-800 border-gray-700 text-gray-400 placeholder-gray-500 focus:ring-2 focus:outline-none focus:ring-gray-700 shadow-slate-600 text-base  focus:outline-double"
+                      className={`${statics.styles.formInputs} border rounded-sm `}
                       id="comment"
                       placeholder="Enter your message"
                       name="message"
@@ -52,14 +77,11 @@ export default function Contact() {
                   </label>
                 </div>
                 <div className="col-span-2 text-right">
-                  <button
-                    type="submit"
-                    className="py-2 px-4  bg-slate-800 border border-gray-700 text-gray-400 hover:text-gray-400 hover:border-gray-600 hover:scale-105 focus:ring-offset-indigo-200 w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md rounded-lg focus:outline-none focus:ring-2 focus:ring-offset-2  "
-                  >
+                  <button type="submit" className={statics.styles.formButton}>
                     Send
                   </button>
                 </div>
-              </div>
+              </motion.div>
             </div>
           </form>
         </div>
