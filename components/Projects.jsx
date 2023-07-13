d29f43aad9e97a5c6606836588e5b4a6beb58af9
aleraiz <alejandroraiz7@gmail.com>
2023-07-13 00:08:18 -0300

    border radios - project section

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -140,13 +140,13 @@ const projects = [
 export default function Projects() {
   return (
     <div
-      className="w-full min-h-screen px-8 md:px-20 pt-32 md:pt-20 py-8 lg:py-20 "
+      className="w-full min-h-screen px-8 md:px-20 pt-32 md:pt-20 py-8 lg:py-20 rounded-lg"
       id="projects"
     >
       <div className="flex items-center justify-center">
         <h3 className="text-4xl">Projects</h3>
       </div>
-      <div className="mt-8 md:mt-20 md:p-10 xl:px-50 w-full 2xl:w-[80%] 2xl:mx-auto flex flex-col flex-wrap h-auto items-center justify-around gap-20 text-gray-400">
+      <div className="mt-8 md:mt-20 md:p-10 xl:px-50 w-full 2xl:w-[80%] 2xl:mx-auto flex flex-col flex-wrap h-auto items-center justify-around gap-20 text-gray-400 rounded-lg">
         {projects.map((project, i) => {
           return <Project project={project} key={project.imgSrc + i} />;
         })}
