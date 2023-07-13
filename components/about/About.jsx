a123442b459e51288456ce2a6f8224ce8e0f5490
aleraiz <alejandroraiz7@gmail.com>
2023-07-13 00:16:39 -0300

    padding and margin - about

--- components/about/About.jsx
+++ components/about/About.jsx
@@ -6,16 +6,13 @@ import { motion, useInView, useAnimation } from "framer-motion";
 
 export default function About() {
   return (
-    <div
-      className="w-full px-8 md:px-20 pt-32 md:pt-20 py-8 lg:py-20 "
-      id="about"
-    >
+    <div className="w-full px-8 md:px-20  md:pt-20 py-8 lg:py-20 " id="about">
       {/* <div
       className="w-full min-h-screen px-8 md:px-20 pt-32 md:pt-20 py-8 lg:py-20 "
       id="about"
     > */}
       <div className="flex items-center justify-center text-light">
-        <h3 className="text-4xl">About me</h3>
+        <h3 className="text-4xl my-10">About me</h3>
       </div>
       <motion.div
         initial={{ opacity: 0, scale: 0 }}
@@ -23,7 +20,7 @@ export default function About() {
         viewport={{ once: true, amount: 0.5 }}
         transition={{ duration: 1 }}
         // className="mt-8 md:mt-20 md:p-20 xl:px-56 text-gray-400"
-        className="md:p-20 xl:px-56 text-gray-400"
+        className="md:px-20 xl:px-56 text-gray-400"
       >
         <p className="my-2">
           I'm Alejandro, a very positive and highly motivated Full Stack
